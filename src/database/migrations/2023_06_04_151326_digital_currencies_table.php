fcfc68f8675e8df97ee5fa9909324e40b37ddad9
A-238 <agafumiya@gmail.com>
2023-06-05 02:09:53 +0900

    暗合通貨テーブル作成
    ・マイグレーションファイル作成

---
+++ src/database/migrations/2023_06_04_151326_digital_currencies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('digital_currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('通貨名');
+            $table->string('symbol')->comment('シンボル');
+            $table->decimal('price', 20, 8)->comment('市場価格');
+            $table->decimal('market_cap', 20, 8)->comment('時価総額');
+            $table->integer('market_cap_rank')->comment('時価総額ランク');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('digital_currencies');
+    }
+};
